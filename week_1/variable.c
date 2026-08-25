619cfa7633c9f59fcd6b257a89c8c1da71569c87
Toky <tokybear2003@gmail.com>
2026-08-25 07:06:23 +0700

    variable's information update

--- week_1/variable.c
+++ week_1/variable.c
@@ -2,7 +2,10 @@
 
 int main(void)
 {
-    //integer variable
+    // These variables which have been declared are initial value 
+    // The process is call initialization. It's done by assignment operator "="
+    
+    // integer variable
     int integer = 6;
 
     // floating-point variable
@@ -16,6 +19,9 @@ int main(void)
     printf("Random integer number: %d\n", integer);
     printf("Random floating number: %.1f\n", floating_point);
     printf("Highest student grade: %c\n", student_grade);
+
+    // To print 2 different types of varible at the same time, we do:
+    printf("%d\n%.1f", integer, floating_point);
 
     // Don't forget return 0 to return the result of the whole main int
     return 0;
